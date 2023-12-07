eac862fa9a5064776aba4d3d0f5dbf366783cb82
Cocopioche <95039203+Cocopioche@users.noreply.github.com>
2023-12-06 19:40:56 -0500

    ban/unban

--- wwwroot/PhotosManager/js/siteUI.js
+++ wwwroot/PhotosManager/js/siteUI.js
@@ -484,14 +484,16 @@ function createUserAdminRow(appendString, user) {
         renderAdmin()
     })
     //Unblock User
-    $(".redCmd").click(() => {
-
-        renderAdmin()
+    $(`.redCmd[data-user-id='${user.Id}']`).click(() => {
+        toggleBan(user).then(() => {
+            renderAdmin()
+        })
     })
     //Block User
-    $(".greenCmd").click(() => {
-
-        renderAdmin()
+    $(`.greenCmd[data-user-id='${user.Id}']`).click(() => {
+        toggleBan(user).then(() => {
+            renderAdmin()
+        })
     })
     //Promote/unpromote
     $(`.dodgerblueCmd[data-user-id="${user.Id}"]`).click((event) => {
@@ -523,8 +525,16 @@ function toggleAdminPromote(profil) {
     )
 }
 
-function toggleBan() {
-
+function toggleBan(profil) {
+    profil.Avatar = ""
+    profil.Password = ""
+    profil.VerifyCode = ""
+    profil.Authorizations.writeAccess = profil.Authorizations.writeAccess === 0 ? profil.Authorizations.readAccess : 0
+    let currentLoggedUser = API.retrieveLoggedUser()
+    return API.modifyUserProfil(profil,currentLoggedUser).then(() => {
+        //function above change the user to profil and i dont want that
+        API.storeLoggedUser(currentLoggedUser)
+    })
 }
 
 
